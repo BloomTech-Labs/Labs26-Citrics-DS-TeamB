1e00f4ca44d73cc3418f433e47cd9a320d453d34
Zack Murray <61975667+zack-murray@users.noreply.github.com>
2020-09-24 13:43:54 -0400

    Update main.py

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.api import predict, viz, cities, population, weather, rental
+from app.api import predict, viz, cities, population, weather, rental, unemployment
 
 app = FastAPI(
     title='CITRICS-TEAM-B DS API',
@@ -16,6 +16,7 @@ app.include_router(viz.router)
 app.include_router(cities.router)
 app.include_router(population.router)
 app.include_router(weather.router)
+app.include_router(unemployment.router)
 
 app.include_router(rental.router)
 
