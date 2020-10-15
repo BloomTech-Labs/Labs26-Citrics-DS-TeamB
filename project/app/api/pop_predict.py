f46a16a465ffd855abb7deed1bdcb79b3bfd0d5e
Zack Murray <61975667+zack-murray@users.noreply.github.com>
2020-10-15 14:04:53 -0400

    Update pop_predict.py
    Split up functions, only returns total population and population density now instead of every metric.

--- project/app/api/pop_predict.py
+++ project/app/api/pop_predict.py
@@ -10,8 +10,8 @@ router = APIRouter()
 
 DATA_FILEPATH1 = os.path.join(os.path.dirname(__file__), "..", "..","data", "historical_pop_complete.csv")
 
-@router.post('/pop_predict/{city_id}')
-async def pop_tse(city_id: int, metric: str):
+@router.post('/pop_predict')
+async def pop_tse(city_id: int):
     """
     Return time series estimates for total population over the next 5 years
 
@@ -19,18 +19,13 @@ async def pop_tse(city_id: int, metric: str):
 
     - `city_id`: [city_id], unique numeric mapping (ex: 0 returns Anchorage, AK)
 
-    - `metric`: 'total_pop', 'land_area', 'pop_density', 'male_pop', 'female_pop',
-    'age_under_20', 'age_20-29', 'age_30-39', 'age_40-49', 'age_50-59', or 'age_above_60';
-    case sensitive, total/male/female pop in thousands, land area
-    in sq mi, pop_density in person/sqmi, age demographics in percentages
-
     ### Response
     Dictionary object 
     """
     rt_dict = {}
     df = pd.read_csv(DATA_FILEPATH1, encoding='utf-8')
     dataframe = df[df['city_id']==city_id]
-    dataframe = dataframe[['year', metric]]
+    dataframe = dataframe[['year', 'total_pop']]
     dataframe.columns = ['ds', 'y']
     dataframe['ds'] = pd.to_datetime(dataframe['ds'], format='%Y')
 
@@ -40,13 +35,40 @@ async def pop_tse(city_id: int, metric: str):
     forecast = m.predict(future)
     data = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
     rt_data = data.to_json(orient='records', date_format='iso')
-    rt_dict["predict_data"] = rt_data
-    rt_dict["predict_viz"] = pop_tse_viz(city_id=city_id, metric=metric)
+    rt_dict["data"] = {"total_pop":rt_data,
+                       "pop_density":pop_dens_tse(city_id=city_id)}
+    rt_dict["viz"] = {"total_pop":tot_pop_tse_viz(city_id=city_id),
+                      "pop_density":pop_dens_tse_viz(city_id=city_id)}
     return rt_dict
-    
-def pop_tse_viz(city_id: int, metric: str):
+
+def pop_dens_tse(city_id: int):
     """
-    Visualize future population estimates for 2020-2024
+    Return time series estimates for total population over the next 5 years
+
+    ### Query Parameters: 
+
+    - `city_id`: [city_id], unique numeric mapping (ex: 0 returns Anchorage, AK)
+
+    ### Response
+    Dictionary object 
+    """
+    df = pd.read_csv(DATA_FILEPATH1, encoding='utf-8')
+    dataframe = df[df['city_id']==city_id]
+    dataframe = dataframe[['year', 'pop_density']]
+    dataframe.columns = ['ds', 'y']
+    dataframe['ds'] = pd.to_datetime(dataframe['ds'], format='%Y')
+
+    m = Prophet()
+    m.fit(dataframe)
+    future = m.make_future_dataframe(periods=5, freq= 'y')
+    forecast = m.predict(future)
+    data = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+    rt_data = data.to_json(orient='records', date_format='iso')
+    return rt_data
+    
+def tot_pop_tse_viz(city_id: int):
+    """
+    Visualize future total population estimates for 2020-2024
 
     ### Query Parameters: 
 
@@ -57,7 +79,8 @@ def pop_tse_viz(city_id: int, metric: str):
     """
     df = pd.read_csv(DATA_FILEPATH1, encoding='utf-8')
     dataframe = df[df['city_id']==city_id]
-    dataframe = dataframe[['year', metric]]
+    dataframe = dataframe[['year', 'total_pop']]
+    dataframe.columns = ['ds', 'y']
     dataframe.columns = ['ds', 'y']
     dataframe['ds'] = pd.to_datetime(dataframe['ds'], format='%Y')
 
@@ -67,6 +90,32 @@ def pop_tse_viz(city_id: int, metric: str):
     forecast = m.predict(future)
     fig = plot_plotly(m, forecast)
     return fig.to_json()
+
+def pop_dens_tse_viz(city_id: int):
+    """
+    Visualize future population density estimates for 2020-2024
+
+    ### Query Parameters: 
+
+    - `city_id`: [city_id], unique numeric mapping (ex: 0 returns Anchorage, AK) 
+
+    ### Response
+    JSON string to render with react-plotly.js
+    """
+    df = pd.read_csv(DATA_FILEPATH1, encoding='utf-8')
+    dataframe = df[df['city_id']==city_id]
+    dataframe = dataframe[['year', 'pop_density']]
+    dataframe.columns = ['ds', 'y']
+    dataframe.columns = ['ds', 'y']
+    dataframe['ds'] = pd.to_datetime(dataframe['ds'], format='%Y')
+
+    m = Prophet()
+    m.fit(dataframe)
+    future = m.make_future_dataframe(periods=5, freq= 'y')
+    forecast = m.predict(future)
+    fig = plot_plotly(m, forecast)
+    return fig.to_json()
+
 
 
 
