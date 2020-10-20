bee88162ffc10cf8dee4b6add2e5a6d86f62147a
Zack Murray <61975667+zack-murray@users.noreply.github.com>
2020-10-20 14:36:26 -0400

    Update pop_predict.py

--- project/app/api/pop_predict.py
+++ project/app/api/pop_predict.py
@@ -35,8 +35,8 @@ async def pop_tse(city_id: int):
     rt_data_dict["city_state"] = rt_data[0][3]
     
     rt_dict["data"] = {"data":rt_data_dict}
-    rt_dict["viz"] = {"total_pop":tot_pop_tse_viz(city_id=rt_data1[0][0]),
-                      "pop_density":pop_dens_tse_viz(city_id=rt_data1[0][0])}
+    rt_dict["viz"] = {"total_pop":tot_pop_tse_viz(city_id=rt_data[0][0]),
+                      "pop_density":pop_dens_tse_viz(city_id=rt_data[0][0])}
     return rt_dict
 
 def tot_pop_tse_viz(city_id: int):
